2775437aea80818fd5d464f39690c7190b6c3d19
blu133721 <tiganinpungasrl@gmail.com>
2023-10-30 15:28:41 +0200

    pls

--- Skin.cpp
+++ Skin.cpp
@@ -56,7 +56,7 @@ UTIL_ClientPrintAll_t FnUTIL_ClientPrintAll;
 void (*FnEntityRemove)(CGameEntitySystem*, void*, void*,uint64_t) = nullptr;
 void (*FnGiveNamedItem)(void* itemService,const char* pchName, void* iSubType,void* pScriptItem, void* a5,void* a6) = nullptr;
 
-void FnGiveNamedItem(CPlayer_ItemServices* pItemServices, const char* pszItemName, const char* pszWeaponName, const char* pszAmmoName, const char* pszAmmo2Name, const char* pszAddonName)
+void FnGiveNamedItem(void* pItemServices, const char* pszItemName, const char* pszWeaponName, const char* pszAmmoName, const char* pszAmmo2Name, const char* pszAddonName)
 {
     if (!pItemServices) return;
 
